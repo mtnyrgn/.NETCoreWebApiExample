05afd9c3e9bc1831d2e04efaac1bf15ea995a69a
mtnyrgn <metinyorgun1996@gmail.com>
2020-09-13 23:50:34 +0300

    add: student model added

---
+++ API/Model/StudentModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Model
+{
+    public class StudentModel
+    {
+        public string StudentIdentityNumber { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
